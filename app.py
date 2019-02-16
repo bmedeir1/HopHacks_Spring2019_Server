a04ba48dce4577d8338d313424d6c6d5aa35709d
Beatriz Medeiros <bmedeir1@jhu.edu>
2019-02-16 13:48:26 -0500

    created report object on server

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request
+import sqlite3
 
 import requests
 
 app = Flask(__name__)
+
+
+class Report:
+    def __init__(self, type, date, time, latitude, longitude):
+        self.type = type
+        self.date = date
+        self.time = time
+        self.latitude = latitude
+        self.longitude = longitude
 
 
 @app.route('/')
@@ -13,7 +23,12 @@ def hello_world():
 def report():
     print(request.json)
     type = request.json["type"]
-    print(type)
+    date = request.json["date"]
+    time = request.json["time"]
+    latitude = request.json["latitude"]
+    longitude = request.json["longitude"]
+    report = Report(type, date, time, latitude, longitude)
+    print(report)
     return("success")
 
 
